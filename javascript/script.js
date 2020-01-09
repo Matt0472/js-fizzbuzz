b51237717f93a371604eccf3fe45b827d1e57e56
Mattia Pedone <mattiapedone93@gmail.com>
2020-01-09 17:39:07 +0100

    esercizio completo con stampa su console, manca stampa su html

--- javascript/script.js
+++ javascript/script.js
@@ -6,11 +6,27 @@
 // Per i numeri che sono sia multipli di 3
 // che di 5 stampi FizzBuzz.
 // Stampiamo come sempre prima in console e poi su html
-var endNumber = 100;
 
-for (var i = 1; i <= endNumber; i++) {
-  console.log(i);
-  if () {
+// for (var i = 1; i <= 100; i++) {
+//   if (i % 3 == 0) {
+//     console.log('Fizz');
+//   } else if (i % 3 == 0 && i % 5 == 0) {
+//     console.log('Buzz');
+//   } else if (i % 5 == 0) {
+//     console.log('FizzBuzz');
+//   } else {
+//     console.log(i);
+//   }
+// }
 
+for (var i = 1; i < 100; i++) {
+  if (i % 3 == 0 && i % 5 == 0) {
+    console.log('FizzBuzz');
+  } else if (i % 3 == 0) {
+    console.log('Fizz');
+  } else if (i % 5 == 0) {
+    console.log('Buzz');
+  } else {
+    console.log(i);
   }
 }
